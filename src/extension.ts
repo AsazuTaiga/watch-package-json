34a4f9f1ab6e0869e297a676c70dea885f508b6f
Asazu Taiga <taiga.asazu@icloud.com>
2021-09-21 06:04:11 +0900

    実装（とりあえず動く状態）

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const fileSystemWatcher = vscode.workspace.createFileSystemWatcher(
+  "**/*/package.json",
+  false,
+  false,
+  false
+);
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "watch-package-json" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "watch-package-json.watchPackageJson",
+    () => {
+      const terminal = vscode.window.createTerminal("watch-package-json");
+      fileSystemWatcher.onDidChange(async () => {
+        terminal.sendText("npm install");
+      });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('watch-package-json.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from watch-package-json!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
